d4089cf2efa93cd16ec3c93c05c6a2a508218cd9
Diego Schneider <desenvolvedorjava87@gmail.com>
2021-02-11 14:32:20 -0300

    alternado estilizaçao css pelo javascript

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,12 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
 
+var tatiana = document.querySelector('#quarta-paciente');
+console.log(tatiana);
+
+var peso = tatiana.querySelector('.info-peso');
+console.log(peso.textContent);
+
 //calcular imc de um determinado paciente
 //querySelector - pega apenas um elemento, já o querySelectorAll pega 
 //todos os elementos
@@ -27,12 +33,16 @@ for(var i=0; i<pacientes.length;i++){
     if(peso<=0 || peso>=1000){
         tdImc.textContent = "Peso inválido";
         pesoEhValido = false;
+        //O Element .classList é uma propriedade somente leitura,
+        // que retorna uma coleção DOMTokenList ativa dos atributos de classe do elemento.
+        paciente.classList.add('paciente-invalido');
     }
 
 
     if(altura<=0 || altura>=3.00){
         tdImc.textContent = "Peso inválido";
         alturaEhValida = false;
+        paciente.classList.add('paciente-invalido');
     }
 
     if(pesoEhValido && alturaEhValida){
